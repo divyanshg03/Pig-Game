411bea0d16afb0addd839ed26ccdceaf19087010
Divyansh Gupta <divyansh_2301me17@iitp.ac.in>
2025-01-07 22:28:50 +0530

    hold Button

--- script.js
+++ script.js
@@ -44,4 +44,28 @@ const dicenum = Math.trunc(Math.random() * 6) + 1;
     player1.classList.toggle('player--active');
   }
 });
+btnhold.addEventListener('click', function()
+{
+    scores[activeplayer] += currentscore;
+    document.getElementById('score--' + activeplayer).textContent = scores[activeplayer];
+    if(scores[activeplayer] >= 100)
+    {
+        document.querySelector('.player--' + activeplayer).classList.add('player--winner');
+        document.querySelector('.player--' + activeplayer).classList.remove('player--active');
+        dice.classList.add('hidden');
+        btnroll.classList.add('hidden');
+        btnhold.classList.add('hidden');
+    }
+    else
+    {
+        document.getElementById('current--' + activeplayer).textContent = 0;
+        currentscore = 0;
+        activeplayer = activeplayer === 0 ? 1 : 0;
+        player0.classList.toggle('player--active');
+        player1.classList.toggle('player--active');
+    }
+});
+
+
+
 
