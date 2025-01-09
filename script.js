f043e7a6f22dce77c3b9754e7fb84073e51778e4
Divyansh Gupta <divyansh_2301me17@iitp.ac.in>
2025-01-09 15:24:28 +0530

    A Working Reset Button

--- script.js
+++ script.js
@@ -22,10 +22,12 @@ const btnhold = document.querySelector('.btn--hold');
 let currentscore =0;
 let scores = [0, 0];
 let activeplayer = 0;
-
+let playing = true;
 
 btnroll.addEventListener('click', function()
 {
+    if(playing)
+    {
 const dicenum = Math.trunc(Math.random() * 6) + 1;
  dice.classList.remove('hidden');
  dice.src = `dice-${dicenum}.png`;
@@ -43,13 +45,21 @@ const dicenum = Math.trunc(Math.random() * 6) + 1;
     player0.classList.toggle('player--active');
     player1.classList.toggle('player--active');
   }
+}
 });
 btnhold.addEventListener('click', function()
 {
+    if(playing)
+    {
+
     scores[activeplayer] += currentscore;
     document.getElementById('score--' + activeplayer).textContent = scores[activeplayer];
+   
     if(scores[activeplayer] >= 100)
     {
+        dice.classList.add('hidden');
+        playing = false;
+      
         document.querySelector('.player--' + activeplayer).classList.add('player--winner');
         document.querySelector('.player--' + activeplayer).classList.remove('player--active');
         dice.classList.add('hidden');
@@ -62,10 +72,26 @@ btnhold.addEventListener('click', function()
         currentscore = 0;
         activeplayer = activeplayer === 0 ? 1 : 0;
         player0.classList.toggle('player--active');
-        player1.classList.toggle('player--active');
+        player1.classList.toggle('player--active'); 
     }
+}
 });
 
-
-
-
+btnnew.addEventListener('click', function()
+{
+    playing = true;
+    scores = [0, 0];
+    currentscore = 0;
+    activeplayer = 0;
+    score0.textContent = 0;
+    score1.textContent = 0;
+    current0.textContent = 0;
+    current1.textContent = 0;
+    player0.classList.remove('player--winner');
+    player1.classList.remove('player--winner');
+    player0.classList.add('player--active');
+    player1.classList.remove('player--active');
+    dice.classList.remove('hidden');
+    btnroll.classList.remove('hidden');
+    btnhold.classList.remove('hidden');
+});
